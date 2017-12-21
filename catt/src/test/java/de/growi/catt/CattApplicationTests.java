bc27efd9639c77f63fa4508e0c48b0d56277576c
growi <growi@ankertaugasse.de>
2017-12-21 21:23:58 +0100

    init

---
+++ catt/src/test/java/de/growi/catt/CattApplicationTests.java
@@ -0,0 +1,16 @@
+package de.growi.catt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CattApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
